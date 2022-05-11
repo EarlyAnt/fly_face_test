494503fd7a0ee85e5eadd6eb967d14d870406c07
EarlyAnt <54763755@qq.com>
2022-05-11 12:51:33 +0800

    1.hello_world.py，添加了upload/after接口和label/desc接口的压测。

--- locust/hello_world.py
+++ locust/hello_world.py
@@ -1,10 +1,9 @@
-from cProfile import label
-from locust import HttpLocust, HttpUser, TaskSet, task, run_single_user
 import json
 
-from requests import Response
+from cProfile import label
+from locust import HttpLocust, HttpUser, TaskSet, SequentialTaskSet, task, run_single_user
 
-class HelloWorld(TaskSet):
+class HelloWorld(SequentialTaskSet):
     label_sn = ""
     
     def on_start(self):
@@ -18,7 +17,7 @@ class HelloWorld(TaskSet):
     #     self.client.get("/")
     
     @task(1)
-    def test_upload_photo(self):
+    def test_upload_before(self):
         url = "api/upload/before"
         header = {
             "mid": "MEDDY133310203062506021"
@@ -29,15 +28,50 @@ class HelloWorld(TaskSet):
         }
         
         response = self.client.post(url=url, data=json.dumps(body), headers=header)
-        print(response)
+        # print("upload/before->response: {}".format(response))
         
         if response.text is not None:
             result = json.loads(response.text)
+            print("upload/before->result: {}".format(result))
             if "data" in result.keys():
                 global label_sn
                 label_sn = result["data"]["label_sn"]
-                print("->label_sn: {}".format(label_sn))
+                # print("->label_sn: {}".format(label_sn))
+                
+    @task(1)
+    def test_upload_after(self):
+        url = "api/upload/after"
+        header = {
+            "mid": "MEDDY133310203062506021"
+        }
+        body = {
+            "label_sn": label_sn
+        }
         
+        response = self.client.post(url=url, data=json.dumps(body), headers=header)
+        # print("upload/after->response: {}".format(response))
+        
+        if response.text is not None:
+            result = json.loads(response.text)
+            print("upload/after->result: {}".format(result))
+            
+    @task(1)
+    def test_label_desc(self):
+        url = "api/label/desc"
+        header = {
+            "mid": "MEDDY133310203062506021"
+        }
+        body = {
+            "sn": label_sn
+        }
+        
+        response = self.client.post(url=url, data=json.dumps(body), headers=header)
+        # print("label/desc->response: {}".format(response))
+        
+        if response.text is not None:
+            result = json.loads(response.text)
+            print("label/desc->result: {}".format(result))
+
 class WebsiteUser(HttpUser):
     tasks = [HelloWorld,]
     # host = "http://www.baidu.com"
@@ -47,12 +81,12 @@ class WebsiteUser(HttpUser):
     max_wait = 5000
 
 if __name__ == "__main__":
-    # 启动方式1
-    run_single_user(WebsiteUser)
+    # # 启动方式1
+    # run_single_user(WebsiteUser)
     
-    # # 启动方式2
-    # import os
-    # os.system("locust -f locust/hello_world.py")
+    # 启动方式2
+    import os
+    os.system("locust -f locust/hello_world.py")
     
     
     
