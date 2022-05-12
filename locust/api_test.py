95edf0b56378f6384a60313afba338ac1f83d33b
EarlyAnt <54763755@qq.com>
2022-05-12 18:44:04 +0800

    1.api_test.py，加上了try-catch。

--- locust/api_test.py
+++ locust/api_test.py
@@ -41,7 +41,7 @@ def start_work():
               [sg.Text('功能说明：选择测试图片做在的目录，然后进行压力测试', text_color="blue")]
              ]
 
-    window = sg.Window('压力测试工具({})'.format(version), layout, resizable=True)
+    window = sg.Window('前端接口压力测试({})'.format(version), layout, resizable=True)
 
     while True:
         event, values = window.read()
@@ -68,12 +68,13 @@ def start_work():
         
     window.close()
     print(f'You clicked {event}')
-    
+
+# 清理缓存
 def clean_cache():
     os.system("sh {}/bld_cleanup.sh".format(os.getcwd()))
 
 # 接口压测
-class HelloWorld(SequentialTaskSet):
+class StressTestingTask(SequentialTaskSet):
     label_sn = ""
     
     def on_start(self):
@@ -87,7 +88,7 @@ class HelloWorld(SequentialTaskSet):
     def test_upload_before(self):
         image_path = get_random_image()
         
-        url = "api/upload/before"
+        url = "api/upload/before?mock_api=1"
         header = {
             "mid": "MEDDY133310203062506021"
         }
@@ -98,19 +99,20 @@ class HelloWorld(SequentialTaskSet):
         }
         
         response = self.client.post(url=url, data=json.dumps(body), headers=header)
-        # print("upload/before->response: {}".format(response))
-        
-        if response.text is not None:
-            result = json.loads(response.text)
-            print("upload/before->result: {}".format(result))
-            if "data" in result.keys() and "label_sn" in result["data"].keys():
-                global label_sn
-                label_sn = result["data"]["label_sn"]
-                # print("->label_sn: {}".format(label_sn))
+        try:
+            if response.text is not None:
+                result = json.loads(response.text)
+                print("upload/before->result: {}".format(result))
+                if "data" in result.keys() and "label_sn" in result["data"].keys():
+                    global label_sn
+                    label_sn = result["data"]["label_sn"]
+                    # print("->label_sn: {}".format(label_sn))
+        except:
+            print("upload/before->response: {}".format(response))
                 
     @task(1)
     def test_upload_after(self):
-        url = "api/upload/after"
+        url = "api/upload/after?mock_api=1"
         header = {
             "mid": "MEDDY133310203062506021"
         }
@@ -119,17 +121,18 @@ class HelloWorld(SequentialTaskSet):
         }
         
         response = self.client.post(url=url, data=json.dumps(body), headers=header)
-        # print("upload/after->response: {}".format(response))
-        
-        if response.text is not None:
-            result = json.loads(response.text)
-            print("upload/after->result: {}".format(result))
+        try:
+            if response.text is not None:
+                result = json.loads(response.text)
+                print("upload/after->result: {}".format(result))
+        except:
+            print("upload/after->response: {}".format(response))
             
     @task(1)
     def test_label_desc(self):
         global label_sn
         
-        url = "api/label/desc"
+        url = "api/label/desc?mock_api=1"
         header = {
             "mid": "MEDDY133310203062506021"
         }
@@ -138,18 +141,18 @@ class HelloWorld(SequentialTaskSet):
         }
         
         response = self.client.post(url=url, data=json.dumps(body), headers=header)
-        # print("label/desc->response: {}".format(response))
-        
-        if response.text is not None:
-            result = json.loads(response.text)
-            print("label/desc->result: {}".format(result))
+        try:
+            if response.text is not None:
+                result = json.loads(response.text)
+                print("label/desc->result: {}".format(result))
+        except:
+            print("label/desc->response: {}".format(response))
 
 # 压测配置
 class WebsiteUser(HttpUser):
-    tasks = [HelloWorld,]
-    # host = "https://mcd.gululu.com/" # web entrance
-    # host = "https://mcd-api.tst.gululu.com/" # api test
-    host = "https://mcd-api.gululu.com/" # api release
+    tasks = [StressTestingTask,]
+    # host = "https://mcd-api.tst.gululu.com/" # test env
+    host = "https://mcd-api.gululu.com/" # release env
     min_wait = 1000
     max_wait = 5000
 
@@ -159,7 +162,6 @@ if __name__ == "__main__":
     # run_single_user(WebsiteUser)
     
     # # 启动方式2
-    # import os
     # os.system("locust -f {}/api_test.py".format(os.path.dirname(__file__)))
     
     # 启动方式3
